c806964111e4bc08e724125e528cef7abce29b8c
aibayanyu20 <zhuzhengjian@powerpms.com>
2026-01-08 17:49:28 +0800

    chore: add dayjs plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,13 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import unocss from "unocss/vite"
+import dayjs from 'vite-plugin-dayjs'
 
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
+    dayjs(),
     vue(),
     unocss(),  
   ],
