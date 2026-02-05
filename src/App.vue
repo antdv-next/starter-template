34afd888f2dfeaa4a0d8cef0b538b61491d1a995
aibayanyu20 <zhuzhengjian@powerpms.com>
2026-02-05 08:52:50 +0800

    chore: update deps

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,15 @@
 <script setup lang="ts">
 import { AimOutlined } from "@antdv-next/icons"
 import { shallowRef } from "vue";
+import { message } from "antdv-next"
+
 const date = shallowRef()
+const handleClick = () => {
+  message.success({
+    content: "操作成功",
+    duration: 5
+  })
+}
 </script>
 
 <template>
@@ -9,7 +17,7 @@ const date = shallowRef()
     <a-app class="w-full px-10">
       <a class="c-primary">测试</a>
       <AimOutlined />
-      <a-button type="primary">测试</a-button>
+      <a-button type="primary" @click="handleClick">测试</a-button>
       <a-divider />
       <a-alert type="info" message="This is an info alert" :show-icon="true"></a-alert>
       <a-date-picker v-model:value="date"></a-date-picker>
