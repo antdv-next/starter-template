cb4f573a32c7e37d5103eeea3b80e4b75c5eefc5
aibayanyu20 <aibayanyu@qq.com>
2026-02-12 16:59:06 +0800

    chore: update  change

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,6 @@
 import { AimOutlined } from "@antdv-next/icons"
 import { shallowRef } from "vue";
 import { message } from "antdv-next"
-
 const date = shallowRef()
 const handleClick = () => {
   message.success({
@@ -20,7 +19,7 @@ const handleClick = () => {
       <a-button type="primary" @click="handleClick">测试</a-button>
       <a-divider />
       <a-alert type="info" message="This is an info alert" :show-icon="true"></a-alert>
-      <a-date-picker v-model:value="date"></a-date-picker>
+      <a-date-picker  v-model:value="date"></a-date-picker>
       <a-flex gap="12px" wrap>
         <!--        -->
       </a-flex>
